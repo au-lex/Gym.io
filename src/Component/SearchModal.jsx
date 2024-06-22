2ae52b1778c4781ea6bce015446f4bbea4ac1d4b
au-lex <aulexc4d2@gmail.com>
2024-06-22 13:47:51 +0100

    search ui done

--- src/Component/SearchModal.jsx
+++ src/Component/SearchModal.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './Header';
 import Footer from './Footer';
+import { BsChevronLeft } from 'react-icons/bs';
 
 const SearchModal = ({isopen, onclose, children}) => {
 
@@ -8,15 +9,15 @@ const SearchModal = ({isopen, onclose, children}) => {
   return (
 
     <>
-    <div className="fixed pt- inse 0  w-full  bottom-0 z-50 flex  h-auto bg-white top-0 itemscenter justify-center  overflow-y-auto">
+    <div className="fixed pt- inse 0  w-full  bottom-0 z-50 flex overflow-hidden  h-auto bg-white top-0 itemscenter justify-center  overflow-y-auto">
     <Header />
         
       <div className="absolute inset-0 bg-black opacity-90" ></div>
-      <div className="bg-white  w-full  pt-[4rem]   z-10">
-        <div className="px-4 mt-8">
+      <div className="bg-white  w-full  pt-[2rem]   z-10">
+        <div className="px- md:px-4 mt-8 mb-[rem]">
 
-         <button onClick={onclose} className="mt-4 bg-yellow-500 text-white px-4 py-1 rounded">
-          Back
+         <button onClick={onclose} className="mt-4 mb-4 font-bold text-red-500 bg-yelow-500 tex-white px-4 py-1 rounded">
+         X
         </button>
         </div>
         {children}
